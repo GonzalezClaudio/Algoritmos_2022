89975251389fb065c95d1f01a857007055978937
Claudio Gonzalez <claudioggonzalez3@gmail.com>
2022-03-22 17:50:12 -0300

    agregue funciones

--- inicio.py
+++ inicio.py
@@ -1,1 +1,59 @@
-print ("hola")
+#para mostrar en pantalla
+  #print ("hola")
+
+#para mostrar texto y variables
+#num=5
+#num2= "hola"
+
+     #print("hola" , num , "chau")
+
+# para pedir que ingrese un dato es con la palabra input, siempre ingresa un string
+       # variable= input ("ingrese su nombre")
+      #print (variable)
+
+#type es una funcion que te dice de que tipo es la variable ej:
+      #print (type(num))
+
+
+#convercion: como input simepre te pone el dato como string, para poder transformar
+#            ese dato como entero se debe utilizar:
+     #variable= int (imput ("ingrese su numero"))
+
+#condicionales:
+#numero=5
+#if (numero>0):
+#    print("es mayor")
+#    print ("gama verdadera")
+#else:
+#    print("es menor")
+
+#print ("fin del codigo")
+
+# cuando ponemos if else se reemplaza por elif:
+#if (numero>0):
+#    print("es positivo")
+#elif (numero < 0):
+#    print ("es negativo")
+#else:
+#    print ("es neutro")
+
+#print("fin del codigo")
+
+#ciclos:
+#while:
+#while (numero>0):
+#    print(numero)
+#    numero -= 1
+
+#for
+#for i in range (1,11): #arranca en 0 (por lo tanto va a ir hasta el 10)
+#    print("interaccion" , i)
+
+#Funciones: (en python noexiste procedimiento)
+# una funcion se define con la palabra reservada def seguido al nombre de la funcion
+numero=4
+def suma(num1,num2): """esta funcion realiza la suma entre dos numeros"""
+    print ("la suma es:" , num1+num2)
+
+#llamamos a la funcion por su nombre ej:
+suma (numero , 5)
--- recursividad.py
+++ recursividad.py
@@ -1,1 +1,41 @@
-print("gggg")
+# FACTORIAL INTERACTIVO:
+#def factorial_i(numero):
+#    factorial=1
+#    for i in range (2,6):
+#        factorial = factorial*i
+    
+#    return factorial
+
+#print (factorial_i(5))
+
+#FACTORIAL RECURSIVO:
+
+#def factorial_r(numero):
+#    if (numero==1 or numero==0):
+#        return 1
+#    else:
+#        return numero*factorial_r(numero-1)
+
+#print (factorial_r(5))
+
+#FIBONACCI INTERACTIVO:
+#def fibonacci_i(numero):
+#    fib_1=0
+#    fib_2=1
+#    fibonacci=0
+#    for i in range (2,numero+1):
+#        fibonacci=fib_1+fib_2
+#        fib_1=fib_2
+#        fib_2=fibonacci
+#    return fibonacci
+
+#print(fibonacci_i(7))
+
+#FIBONACCI RECURSIVO:
+def fibonacci_r(numero):
+    if(numero==0 or numero==1):
+        return numero
+    else:
+        return fibonacci_r(numero-1) + fibonacci_r (numero-2)
+
+print (fibonacci_r(8))
