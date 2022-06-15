cfdb3ae788683d480bd027596cd8b5b8310972c5
Claudio Gonzalez <claudioggonzalez3@gmail.com>
2022-06-15 18:09:11 -0300

    archivo examen

---
+++ cola.py
@@ -0,0 +1,46 @@
+class nodoCola():
+    info, sig = None, None
+
+
+class Cola():
+
+    def __init__(self):
+        self.__frente = None
+        self.__final = None
+        self.__tamnio = 0
+
+    def arribo(self, dato):
+        nodo = nodoCola()
+        nodo.info = dato
+
+        if self.__final is None:
+            self.__frente = nodo
+        else:
+            self.__final.sig = nodo
+        self.__final = nodo
+
+        self.__tamnio += 1
+
+    def atencion(self):
+        dato = self.__frente.info
+
+        self.__frente = self.__frente.sig
+        if self.__frente is None:
+            self.__final = None
+
+        self.__tamnio -= 1
+        return dato
+
+    def tamanio(self):
+        return self.__tamnio
+
+    def cola_vacia(self):
+        return self.__frente is None
+
+    def en_frente(self):
+        return self.__frente.info
+
+    def mover_al_final(self):
+        dato = self.atencion()
+        self.arribo(dato)
+        return dato
